da93e0cdbcc2fbb4180743783c6647c48795fd31
Yassel <sioy23@gmail.com>
2022-09-24 17:27:01 -0300

    add: Módulo Telesalud

---
+++ telesalud/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Módulo Telesalud en OpenEmr
+ * Descripción del módulo
+ * 
+ *
+ * @package   OpenEMR
+ * @link      http://www.open-emr.org
+ * @author    Yassel Omar Izquierdo Souchay <sioy23@gmial.com>
+ * @author    Alejandro Benavides <alejandro@meddyg.com>
+ * @copyright Copyright (c) 2022 
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE
+ * @license   https://github.com/ciips-code/openemr-telesalud/blob/master/LICENSE
+ * 
+ * 
+ */
+require_once ("./globals.php");
