e447bd8a09734e6fb9f8c4ee6b1feddb3a87f2ad
Yassel <sioy23@gmail.com>
2022-09-30 12:27:00 -0300

    add: API Identificación Profesionales #1

--- telesalud/laravel/routes/api.php
+++ telesalud/laravel/routes/api.php
@@ -1,19 +1,25 @@
 <?php
-
+use App\Http\Controllers\APIV1\TsaludIdProfesionalController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+ * |--------------------------------------------------------------------------
+ * | API Routes
+ * |--------------------------------------------------------------------------
+ * |
+ * | Here is where you can register API routes for your application. These
+ * | routes are loaded by the RouteServiceProvider within a group which
+ * | is assigned the "api" middleware group. Enjoy building your API!
+ * |
+ */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * Aca se aseguran las APIS
+ */
+// Profesionals Ids
+Route::resource('/profesionalsid', TsaludIdProfesionalController::class);
