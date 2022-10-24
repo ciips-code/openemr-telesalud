59e5dd4bcd50bb78d3a5c8c5805274dae3235e49
Alejandro Benavides <alejandro@meddyg.com>
2022-10-24 05:50:17 +0000

    close #9

---
+++ telesalud/api/v1/routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\{AuthController, JitsiController};
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/userinfo', [AuthController::class, 'infouser']);
+    Route::post('/revoke', [AuthController::class, 'revoke']);
+
+    Route::get('patients', [PatientController::class, 'showAllPatients']);
+    Route::get('patient/{id}', [PatientController::class, 'showOnePatient']);
+    Route::get('patient/{id}/encounters', [PatientController::class, 'showAllEncountersByPatient']);
+    Route::get('patient/{id}/encounter/{encounterId}', [PatientController::class, 'showPatientEncounterById']);
+
+    Route::post('vc/create', [JitsiController::class, 'createVC']);
+
+});
