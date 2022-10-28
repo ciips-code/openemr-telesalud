d1b0f7cc75acd0117114a3b332b37f586fcaed7e
YOIS <sioy23@gmail.com>
2022-10-28 14:18:21 -0300

    patch: links teleconsultas en cita

--- interface/main/calendar/add_edit_event.php
+++ interface/main/calendar/add_edit_event.php
@@ -752,8 +752,8 @@ if (!empty($_POST['form_action']) && ($_POST['form_action'] == "save")) {
     /* =======================================================
      *                    TELESALUD 
      * ======================================================*/
-        require_once( $_SERVER['DOCUMENT_ROOT'] . '/telesalud/controllers/C_TSalud_Vc.php');
-        createVc($eid);
+        // require_once( $_SERVER['DOCUMENT_ROOT'] . '/telesalud/controllers/C_TSalud_Vc.php');
+        // createVc($eid);
         /* =======================================================
      *                    END TELESALUD 
      * ======================================================*/
@@ -1793,7 +1793,12 @@ if (empty($_GET['prov'])) { ?>
     <div class="col-sm form-group">
         <!-- <label><?php echo xlt('Comments'); ?>:</label> -->
         <input class='form-control' type='hidden' name='form_comments' value='<?php echo attr($hometext); ?>' title='<?php echo xla('Optional information about this event'); ?>' />
-        <?php echo htmlspecialchars_decode(stripslashes($hometext)); ?>
+        <!-- <?php echo htmlspecialchars_decode(stripslashes($hometext)); ?>⁄ -->
+        Accesos a la video consulta:
+                    <ul>
+                    <li> Profesional: <a href="https://srv3.integrandosalud.com/os-telesalud/videoconsultation?vc=1a1ba7a4e18756f02d939e1f95dbcdb82e744869&medic=KO15fC6fzh" target="_blank">https://srv3.integrandosalud.com/os-telesalud/videoconsultation?vc=1a1ba7a4e18756f02d939e1f95dbcdb82e744869&medic=KO15fC6fzh</a></li>
+                    <li>Paciente: <a href="https://srv3.integrandosalud.com/os-telesalud/videoconsultation?vc=1a1ba7a4e18756f02d939e1f95dbcdb82e744869" target="_blank">https://srv3.integrandosalud.com/os-telesalud/videoconsultation?vc=1a1ba7a4e18756f02d939e1f95dbcdb82e744869</a></li>
+                    </ul>
     </div>
 </div>
 <div class="form-row mx-2">
