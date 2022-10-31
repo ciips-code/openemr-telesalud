077e13a6121fff34f17cae3c8d984717b1ce78ca
Alejandro Benavides <alejandro@meddyg.com>
2022-10-31 22:17:14 +0000

    feat: Refactor en los nombre de las tablas

--- telesalud/api/v1/database/migrations/2022_10_31_000000_create_users_table.php
+++ telesalud/api/v1/database/migrations/2022_10_31_000000_create_users_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('telesa_failed_jobs', function (Blueprint $table) {
+        Schema::create('tsalud_users', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('telesa_failed_jobs');
+        Schema::dropIfExists('tsalud_users');
     }
 };
