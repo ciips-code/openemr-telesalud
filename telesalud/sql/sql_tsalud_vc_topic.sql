d68c8be214892ef6c80f98159d35c67ff5968c3a
YOIS <sioy23@gmail.com>
2022-11-16 00:59:20 -0300

    refactor: estados de video consulta

--- telesalud/sql/sql_tsalud_vc_topic.sql
+++ telesalud/sql/sql_tsalud_vc_topic.sql
@@ -6,14 +6,14 @@ CREATE TABLE `tsalud_vc_topic` (
 );
 
 INSERT INTO `tsalud_vc_topic` VALUES
-('medic-set-attendance','El médico ingresa a la videoconsulta','@@');
+('medic-set-attendance','El médico ingresa a la videoconsulta','-');
 INSERT INTO `tsalud_vc_topic` VALUES
-('medic-unset-attendance','El médico cierra la pantalla de videoconsulta','x');
+('medic-unset-attendance','El médico cierra la pantalla de videoconsulta','>');
 INSERT INTO `tsalud_vc_topic` VALUES
 ('videoconsultation-started','Se da por iniciada la videoconsulta, esto se da cuando tanto el médico como el paciente están presentes
-','@@@');
+','-');
 INSERT INTO `tsalud_vc_topic` VALUES
 ('videoconsultation-finished','El médico presiona el botón Finalizar consulta
-','>');
+','-');
 INSERT INTO `tsalud_vc_topic` VALUES
 ('patient-set-attendance','El paciente anuncia su presencia','@');
