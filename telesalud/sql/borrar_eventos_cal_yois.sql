e7c35cf25df85de3d62cbae1df22bcab0d8a6c66
YOIS <sioy23@gmail.com>
2022-11-16 09:52:15 -0300

    refactor: sql borrar eventos de calendario para yois

--- telesalud/sql/borrar_eventos_cal_yois.sql
+++ telesalud/sql/borrar_eventos_cal_yois.sql
@@ -1,7 +1,12 @@
-delete from openemr_postcalendar_events where pc_pid=10;
+DELETE FROM openemr_postcalendar_events 
+WHERE
+    pc_pid = 10;
 SELECT 
     *
 FROM
     openemr_postcalendar_events
+WHERE
+    pc_pid = 10
 ORDER BY pc_eid DESC
 LIMIT 10;
+
