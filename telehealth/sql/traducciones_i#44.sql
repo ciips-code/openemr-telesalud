441a9ac27763d6b72b5c7823acabd7b85035277f
YOIS <sioy23@gmail.com>
2022-12-05 22:54:07 -0300

    add: query actualizar idioma

---
+++ telehealth/sql/traducciones_i#44.sql
@@ -0,0 +1,72 @@
+INSERT INTO lang_definitions (`cons_id`, `lang_id`, `definition`)
+VALUES (
+		(
+			SELECT lc.cons_id
+			FROM lang_constants AS lc
+			WHERE constant_name = 'Recurring Appointments'
+				AND lc.cons_id NOT IN (
+					SELECT DISTINCT cons_id
+					FROM lang_definitions ld
+					WHERE ld.lang_id = 4
+				)
+			LIMIT 1
+		), 4, 'Citas Recurrentes'
+	);
+INSERT INTO lang_definitions (`cons_id`, `lang_id`, `definition`)
+VALUES (
+		(
+			SELECT lc.cons_id
+			FROM lang_constants AS lc
+			WHERE constant_name = 'No Recurring Appointments'
+				AND lc.cons_id NOT IN (
+					SELECT DISTINCT cons_id
+					FROM lang_definitions ld
+					WHERE ld.lang_id = 4
+				)
+			LIMIT 1
+		), 4, 'Sin Citas Recurrentes'
+	);
+INSERT INTO lang_definitions (`cons_id`, `lang_id`, `definition`)
+VALUES (
+		(
+			SELECT lc.cons_id
+			FROM lang_constants AS lc
+			WHERE constant_name = 'Recurring Appointment'
+				AND lc.cons_id NOT IN (
+					SELECT DISTINCT cons_id
+					FROM lang_definitions ld
+					WHERE ld.lang_id = 4
+				)
+			LIMIT 1
+		), 4, 'Cita Recurrente'
+	);
+INSERT INTO lang_definitions (`cons_id`, `lang_id`, `definition`)
+VALUES (
+		(
+			SELECT lc.cons_id
+			FROM lang_constants AS lc
+			WHERE constant_name = 'Lab Results'
+				AND lc.cons_id NOT IN (
+					SELECT DISTINCT cons_id
+					FROM lang_definitions ld
+					WHERE ld.lang_id = 4
+				)
+			LIMIT 1
+		), 4, 'Resultados de Laboratorio'
+	);
+INSERT INTO lang_constants (constant_name)
+VALUES (' years old');
+INSERT INTO lang_definitions (`cons_id`, `lang_id`, `definition`)
+VALUES (
+		(
+			SELECT lc.cons_id
+			FROM lang_constants AS lc
+			WHERE constant_name = ' years old'
+				AND lc.cons_id NOT IN (
+					SELECT DISTINCT cons_id
+					FROM lang_definitions ld
+					WHERE ld.lang_id = 4
+				)
+			LIMIT 1
+		), 4, ' años de edad'
+	);
