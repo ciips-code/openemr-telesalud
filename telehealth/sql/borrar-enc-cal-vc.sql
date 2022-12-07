b14060299c585d75a115d586a3ce9d5ff92b57c2
YOIS <sioy23@gmail.com>
2022-12-07 06:30:33 -0300

    ref: sql query utiles

--- telehealth/sql/borrar-enc-cal-vc.sql
+++ telehealth/sql/borrar-enc-cal-vc.sql
@@ -1,23 +1,23 @@
 -- BORRAR TODAS LAS AGENDAS
--- truncate table openemr_postcalendar_events;
+truncate table openemr_postcalendar_events;
 -- BORRAR VIDEO CONFERENCIAS 
--- truncate table telehealth_vc;
--- truncate table telehealth_vc_log;
+truncate table telehealth_vc;
+truncate table telehealth_vc_log;
 -- BORRAR TODOS ENCUENTROS
--- truncate table form_encounter;
--- truncate table forms;
+truncate table form_encounter;
+truncate table forms;
 -- BORRAR TODOS LOS DOCUMETOS
--- truncate table categories_to_documents;
--- truncate table documents;
-
-SELECT 
-    *
-FROM
-    telehealth_vc_log
-ORDER BY id DESC;
+truncate table categories_to_documents;
+truncate table documents;
 
 SELECT 
     *
 FROM
     telehealth_vc
 ORDER BY pc_eid DESC;
+SELECT 
+    *
+FROM
+    telehealth_vc_log
+ORDER BY id DESC;
+
