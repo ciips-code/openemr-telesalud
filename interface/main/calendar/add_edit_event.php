cf85951698a1b2ec601bc58c9df266deb3368d8f
YOIS <sioy23@gmail.com>
2022-12-14 11:19:14 -0300

    ref: cleanup code

--- interface/main/calendar/add_edit_event.php
+++ interface/main/calendar/add_edit_event.php
@@ -2069,21 +2069,6 @@ function SubmitForm() {
             // alert("Copied:" + linktag.href);
         }
     }
-
-//     const unsecuredCopyToClipboard = (text) => { const textArea = document.createElement("textarea"); textArea.value=text; document.body.appendChild(textArea); textArea.focus();textArea.select(); try{document.execCommand('copy')}catch(err){console.error('Unable to copy to clipboard',err)}document.body.removeChild(textArea)};
-
-// /**
-//  * Copies the text passed as param to the system clipboard
-//  * Check if using HTTPS and navigator.clipboard is available
-//  * Then uses standard clipboard API, otherwise uses fallback
-// */
-// const copyToClipboard = (content) => {
-//   if (window.isSecureContext && navigator.clipboard) {
-//     navigator.clipboard.writeText(content);
-//   } else {
-//     unsecuredCopyToClipboard(content);
-//   }
-// };
 // return a promise
 function copyToClipboard(textToCopy) {
     // navigator clipboard api needs a secure context (https)
