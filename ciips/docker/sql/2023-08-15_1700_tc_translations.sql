b8a7cf1dd405f8c41799c2b7533f0538fa08b429
Ricardo <ricardosel@gmail.com>
2023-08-16 17:25:52 -0300

    OST-19

---
+++ ciips/docker/sql/2023-08-15_1700_tc_translations.sql
@@ -0,0 +1,9 @@
+INSERT INTO openemr.lang_constants (id,constant_name) VALUES
+(13479, 'Video Consultation Links')
+(13480, 'Professional')
+;
+
+INSERT INTO openemr.lang_definitions(cons_id, lang_id, definition) VALUES
+(13479, 4, 'Accesos a la video consulta')
+(13480, 4, 'Profesional')
+;
